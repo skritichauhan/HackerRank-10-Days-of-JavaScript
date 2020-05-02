5f260b0d806882b0af9c782ff96e11c4504ab8c4
Sanskriti Chauhan <skritichauhan@gmail.com>
2020-05-02 09:41:17 +0530

    Day 0: Data Types

--- 10 Days Of JavaScript/Day 0/Data Types.js
+++ 10 Days Of JavaScript/Day 0/Data Types.js
@@ -30,6 +30,6 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     
     
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
-    
+   
 }
 
