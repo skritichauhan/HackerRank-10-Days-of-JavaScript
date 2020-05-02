0e89eed1d45336416835ef92c991c96c3414ef53
Sanskriti Chauhan <skritichauhan@gmail.com>
2020-05-02 22:21:46 +0530

    Create Pointer.cpp

---
+++ C++ Language Proficiency/Introduction/Pointer.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+void update(int *a,int *b) {
+     *a=(*a)+(*b);
+    *b=abs(*a-(2*(*b)));
+    // Complete this function    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
