e4733c29ae5eda523420e6a81ec5d1d77226849d
Sanskriti Chauhan <skritichauhan@gmail.com>
2020-05-02 22:16:11 +0530

    Create Basic Data Types.cpp

---
+++ C++ Language Proficiency/Introduction/Basic Data Types.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    // Complete the code.
+    cin>>i>>l>>c>>f>>d;
+    cout<<i<<endl<<l<<endl<<c<<endl;
+    cout<<fixed<<setprecision(3)<<f<<endl;
+    cout<<fixed<<setprecision(6)<<d;
+    
+    return 0;
+}
+
