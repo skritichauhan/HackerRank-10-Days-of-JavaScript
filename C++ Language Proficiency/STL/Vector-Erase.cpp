f1e043facf7b847a2774be42789badad51277ca9
Irfan <irfnhm@gmail.com>
2020-05-05 22:05:45 +0530

    Adds minor fixes.
    1. Importing whole standard library is a bad practice.
    2. instead of "endl" use "\n", it is faster, less CPU usage.
    3. Range based for loop should be used with "auto".
    
    P.s: I randomly search C++ code on github, then I learn and add my suggestions. :)

--- C++ Language Proficiency/STL/Vector-Erase.cpp
+++ C++ Language Proficiency/STL/Vector-Erase.cpp
@@ -1,34 +1,29 @@
-#include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <ostream>
-using namespace std;
-
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int n,pos,a,b;
-    vector <int> v;
-    cin >> n;
-    int data;
+    std::vector <int> v;
+    std::cin >> n;
     for(int i = 0; i < n; ++i)
     {
-        cin >> data;
+        int data;
+        std::cin >> data;
         v.push_back(data);
     } 
 
-    cin >> pos;
+    std::cin >> pos;
     v.erase(v.begin()+pos-1);
-    cin >> a;
-    cin >> b;
+    std::cin >> a >> b;
     
     v.erase(v.begin() + a - 1, v.begin() + b - 1);
-    cout<<v.size()<<endl;
-    for(int i: v)
+    std::cout << v.size() << "\n";
+    for(auto i: v)
     {
         cout << i << " ";
     }
-    cout << endl;
-    return 0;
+    cout << "\n";
+    //return 0; Modern compilers don't need a return type.
 }
