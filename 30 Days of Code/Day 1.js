483f1c18c191223d016306dd970fb8c3756f2b11
Sanskriti Chauhan <skritichauhan@gmail.com>
2020-05-08 14:42:02 +0530

    Create Day 1.js

---
+++ 30 Days of Code/Day 1.js
@@ -0,0 +1,18 @@
+
+    // Declare second integer, double, and String variables.
+let intgr,numbr,strng;
+intgr=Number(readLine());
+numbr=parseFloat(readLine());
+strng=readLine();
+    // Read and save an integer, double, and String to your variables.
+
+    // Print the sum of both integer variables on a new line.
+console.log(i+intgr);
+    // Print the sum of the double variables on a new line.
+
+var sum=(d+numbr).toFixed(1);
+console.log(sum);
+    // Concatenate and print the String variables on a new line
+console.log(s.concat(strng));
+    // The 's' variable above should be printed first.
+
