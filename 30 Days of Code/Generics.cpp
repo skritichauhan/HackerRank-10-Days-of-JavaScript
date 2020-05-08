014da665f805a8bc25632a9f628a7739660584b8
Sanskriti Chauhan <skritichauhan@gmail.com>
2020-05-08 15:01:58 +0530

    Create Generics.cpp

---
+++ 30 Days of Code/Generics.cpp
@@ -0,0 +1,16 @@
+
+
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+// Write your code here
+template <typename T>
+T printArray(vector<T>v)
+{
+    for(int i=0;i<v.size();i++)
+        cout<<v[i]<<endl;
+        return v[0];
+}
